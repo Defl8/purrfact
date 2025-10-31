cf7602b9e024642cdeb7061e383df10da3a6c3e5
Wyatt James <wyattsamueljames@gmail.com>
2025-10-31 15:21:18 -0600

    feat(database): add database check exists

--- internal/database/db.go
+++ internal/database/db.go
@@ -1,13 +1,39 @@
 package database
 
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"errors"
+)
 
 
-type DB struct {
+
+type Database struct {
+	FullPath string
 	Name string
+	Driver string
+}
 
+func (database Database) SetDBPath(path string) error {
+	if _, err := os.Stat(path); errors.Is(err, os.ErrNotExist) {
+		return fmt.Errorf("Database at path '%s' does not exits. ERROR: %w", path, err)
+	}
 }
 
 
-func (db DB) ExecuteQuery() {
+func (database Database) connect() (*sql.DB, error) {
+	dbConn, err := sql.Open(database.Driver, database.Name)
+	if err != nil {
+		return nil, fmt.Errorf("Connection to '%s' database could not be established. ERROR %w", database.Name, err)
+	}
+	
+	if err := dbConn.Ping(); err != nil {
+		dbConn.Close()
+		return nil, fmt.Errorf("Could not ping '%' database", database.Name, err)
+	}
+}
+
+func (db Database) ExecuteQuery() {
 
 }
