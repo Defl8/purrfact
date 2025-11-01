c24fa6780af25c2b8f0fd05b00c488d7ca07f596
Wyatt James <wyattsamueljames@gmail.com>
2025-10-31 23:06:35 -0600

    feat(database): add ExecuteSelect and ExecuteDUI

--- internal/database/db.go
+++ internal/database/db.go
@@ -19,6 +19,8 @@ func (database Database) SetDBPath(path string) error {
 	if _, err := os.Stat(path); errors.Is(err, os.ErrNotExist) {
 		return fmt.Errorf("Database at path '%s' does not exits. ERROR: %w", path, err)
 	}
+
+	return nil
 }
 
 
@@ -30,10 +32,44 @@ func (database Database) connect() (*sql.DB, error) {
 	
 	if err := dbConn.Ping(); err != nil {
 		dbConn.Close()
-		return nil, fmt.Errorf("Could not ping '%' database", database.Name, err)
+		return nil, fmt.Errorf("Could not ping '%s' database. ERROR %w", database.Name, err)
 	}
+
+	return dbConn, nil
 }
 
-func (db Database) ExecuteQuery() {
+func (db Database) ExecuteSelect(query string) (*sql.Rows, error) {
+	conn, err := db.connect()
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
 
+	rows, err := conn.Query(query)
+	if err != nil {
+		rows.Close()
+		return nil, err
+	}
+
+	return rows, nil
+}
+
+func (db Database) ExecuteDUI(query string) (int64, error) {
+	conn, err := db.connect()
+	if err != nil {
+		return 0, err
+	}
+	defer conn.Close()
+
+	result, err := conn.Exec(query)
+	if err != nil {
+		return 0, err
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return rowsAffected, err
+	}
+
+	return rowsAffected, nil
 }
