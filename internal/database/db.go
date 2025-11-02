41ee4c137bbf9c98b20c901481b297e04ae98516
Wyatt James <wyattsamueljames@gmail.com>
2025-11-02 16:13:43 -0700

    feat(database): add turso connection

--- internal/database/db.go
+++ internal/database/db.go
@@ -2,26 +2,54 @@ package database
 
 import (
 	"database/sql"
-	"fmt"
-	"os"
 	"errors"
-)
+	"fmt"
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
+
+	// Turso driver
+	_ "github.com/tursodatabase/libsql-client-go/libsql"
+)
 
 type Database struct {
 	FullPath string
-	Name string
-	Driver string
+	Name     string
+	Driver   string
+	IsRemote bool
+	Token    string
 }
 
-func NewDatabase(driver string, fullpath string, name string) Database {
-	return Database{
-		Driver: driver,
-		Name:   name,
-		FullPath: fullpath,
+func NewDatabase(name, driver string, isRemote bool) *Database {
+
+	if isRemote {
+		if err := godotenv.Load(); err != nil {
+			log.Fatal(err)
+		}
+
+		url := os.Getenv("TURSO_DATABASE_URL")
+		token := os.Getenv("TURSO_AUTH_TOKEN")
+
+		fullpath := fmt.Sprintf("%s?auth_token=%s", url, token)
+
+		return &Database{
+			Driver:   driver,
+			Name:     name,
+			FullPath: fullpath,
+			IsRemote: isRemote,
+			Token:    token,
+		}
+	}
+
+	return &Database{
+		Driver:   driver,
+		Name:     name,
+		IsRemote: isRemote,
 	}
 }
 
+// Only really use for local db
 func (database Database) SetDBPath(path string) error {
 	if _, err := os.Stat(path); errors.Is(err, os.ErrNotExist) {
 		return fmt.Errorf("Database at path '%s' does not exits. ERROR: %w", path, err)
@@ -30,13 +58,12 @@ func (database Database) SetDBPath(path string) error {
 	return nil
 }
 
-
 func (database Database) connect() (*sql.DB, error) {
-	dbConn, err := sql.Open(database.Driver, database.Name)
+	dbConn, err := sql.Open(database.Driver, database.FullPath)
 	if err != nil {
 		return nil, fmt.Errorf("Connection to '%s' database could not be established. ERROR %w", database.Name, err)
 	}
-	
+
 	if err := dbConn.Ping(); err != nil {
 		dbConn.Close()
 		return nil, fmt.Errorf("Could not ping '%s' database. ERROR %w", database.Name, err)
